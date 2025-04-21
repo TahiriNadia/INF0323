dbb8a044d6cc28f7d266145f27caca497a974939
Nadia Tahiri <Nadia.Tahiri@USherbrooke.ca>
2025-04-20 22:55:52 -0400

    Delete C_MOD_CO3x/TD/create_table_PertMembre.sql

--- C_MOD_CO3x/TD/create_table_PertMembre.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE MembrePret AS
-SELECT Pret.idMembre AS idMembre, nom, telephoneResidence, noSequence, datePret, Pret.idMembre AS idMembrePret, idExemplaire
-FROM Membre
-FULL JOIN Pret ON Membre.idMembre = Pret.idMembre;
